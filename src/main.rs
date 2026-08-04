9d3f52897e3fc8af3cf5089def83b4d5a2050a92
Naga Vijayapuram <nvijayap@gmail.com>
2026-08-04 04:25:26 -0700

    Showing # of args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use futures::executor::block_on;
 use log::info;
 use std::{
-    sync::mpsc::Receiver,
-    sync::mpsc::{self, Sender},
+    env,
+    sync::mpsc::{self, Receiver, Sender},
     thread,
 };
 use uuid::Uuid;
@@ -11,6 +11,9 @@ use uuid::Uuid;
 #[tokio::main]
 async fn main() {
     env_logger::init(); // For env_logger
+
+    let args: Vec<String> = env::args().collect();
+    println!("Number of arguments is {}", args.len());
 
     // Create a channel for "safe concurrency"
     // mpsc: MPSC in Rust stands for "Multi-Producer, Single-Consumer,"
@@ -31,6 +34,7 @@ fn send(tx: Sender<String>) {
         // Simulate a blocking operation (e.g., writing to RustFS)
         // Send data through the channel to comply with and adhere to ACID semantics
         let data = format!("Data to write to RustFS - {:?}", Uuid::new_v4());
+        info!(" Sending: {:?}", data);
         tx.send(data).unwrap();
     });
 }
